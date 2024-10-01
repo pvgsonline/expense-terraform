02328ffdd822ba17e8e55f4eddc42b2fcf956178
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-10-01 12:54:59 +0530

    terraform

---
+++ sg.tf
@@ -0,0 +1,9 @@
+module "sg"{
+    source = "../terraform-aws-security-group"
+    vpc_id = module.vpc.vpc_id
+    project_name = var.project_name
+    enevironment = var.environment
+    common_tags = var.common_tags
+    security_group_name = "mysql"
+
+}
---
+++ vpc.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+    source= "../terraform-aws-vpc"
+    cidr = var.cidr_block
+    common_tags = var.common_tags
+    project_name = var.project_name
+    environment = var.environment
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
+    is_peering_required = var.is_peering_required
+}   
