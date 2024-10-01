e4191193db0ff25c26a79356fe3f92c1b799fdad
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-10-01 13:11:05 +0530

    terraform

--- sg.tf
+++ sg.tf
@@ -2,7 +2,7 @@ module "sg"{
     source = "../terraform-aws-security-group"
     vpc_id = module.vpc.vpc_id
     project_name = var.project_name
-    enevironment = var.environment
+    environment = var.environment
     common_tags = var.common_tags
     security_group_name = "mysql"
 
